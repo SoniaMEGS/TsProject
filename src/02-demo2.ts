3c73acdd236d31101d20fb00d1da13f642c64650
SoniaMEGS <megs.sonia@gmail.com>
2024-05-15 17:24:07 -0700

    Creating TSConfig.json

---
+++ src/02-demo2.ts
@@ -0,0 +1,5 @@
+const numArray = [1, 2, 3];
+console.log(numArray);
+
+//Crea el archivo de configuracion de TS: npx tsc --init
+//Hace que este transpilando por cada cambio guardado: npx tsc --watch
